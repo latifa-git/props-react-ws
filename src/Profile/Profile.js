c4ef0cf3d8b37739b0aa821eb94e8325a13448aa
latifa-git <latifabenameur96@gmail.com>
2021-05-16 20:08:49 +0100

    thirdchange

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,21 +2,22 @@ import React from "react";
 
 
 const Profile = ({ myelementProps, children }) => {
+  const handleClick = (name) => {
+    alert(`the name of the profile ${name}`)
+}
   return (
     <div>
       <h1> Name:{myelementProps.fullName}</h1>
-      
+
       {children}
-      <h6>hello my name is {myelementProps.fullName} i am a {myelementProps.profession
-     } in ISIM gabes , I am so glad to study embedded systems or for short {myelementProps.bio}
+      <h6>
+        hello my name is {myelementProps.fullName} i am a{" "}
+        {myelementProps.profession} in ISIM gabes , I am so glad to study
+        embedded systems or for short {myelementProps.bio}
       </h6>
-   
+      <button onClick={() => handleClick(myelementProps.fullName)}> button</button>
     </div>
   );
-
 };
-
-  
-
 
 export default Profile;
